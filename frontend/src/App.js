cfe2a03362152eee8c166aad380b62477f5617ba
Ricardo Flores <ricardo.flores11@hotmail.com>
2020-11-28 01:52:27 +0000

    Payment Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterSreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 //Ficheiro principal 
 function App() {
@@ -18,6 +21,9 @@ function App() {
         <Container>
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/" component={HomeScreen} exact />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_SUCCESS, USER_REGISTER_REQUEST, USER_REGISTER_FAIL } from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_SUCCESS, USER_REGISTER_REQUEST, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_DETAILS_FAIL } from "../constants/userConstants"
 import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
@@ -63,6 +63,64 @@ export const register = (name, email, phone, password) => async (dispatch) => {
         dispatch(
             {
                 type: USER_REGISTER_FAIL,
+                payload: error.response && error.response.data.message ? error.response.data.message : error.message
+            });
+    }
+}
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_DETAILS_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'Application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`api/users/${id}`, config);
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS, payload: data
+        });
+    } catch (error) {
+        dispatch(
+            {
+                type: USER_DETAILS_FAIL,
+                payload: error.response && error.response.data.message ? error.response.data.message : error.message
+            });
+    }
+}
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'Application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.put(`api/users/profile`, user, config);
+
+        dispatch({
+            type: USER_UPDATE_PROFILE_SUCCESS, payload: data
+        });
+    } catch (error) {
+        dispatch(
+            {
+                type: USER_UPDATE_PROFILE_FAIL,
                 payload: error.response && error.response.data.message ? error.response.data.message : error.message
             });
     }
