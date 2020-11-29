01f4f0d9adfeda85c52981c164ec193e7b716231
Ricardo Flores <ricardo.flores11@hotmail.com>
2020-11-29 23:13:12 +0000

    Order Screen and Update order to paid

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
-import { addOrderItems, getOrderbyId } from '../controllers/orderController.js';
+import { addOrderItems, getOrderbyId, updateOrderToPaid } from '../controllers/orderController.js';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
 
 router.route('/:id').get(protect, getOrderbyId);
+
+router.route('/:id/pay').get(protect, updateOrderToPaid);
 
 export default router;
