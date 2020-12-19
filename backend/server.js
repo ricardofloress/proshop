ee14bf558ace2561d9824b33e0cc64294a15d6dc
Ricardo Flores <ricardo.flores11@hotmail.com>
2020-12-19 20:47:06 +0000

    Express server static problem solved

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
 import colors from 'colors';
-import path from 'path';
+import morgan from 'morgan';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -15,11 +16,11 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-})
+app.use(express.json());
 
 app.use('/api/products', productRoutes);
 
@@ -29,15 +30,33 @@ app.use('/api/orders', orderRoutes);
 
 app.use('/api/upload', uploadRoutes);
 
-app.use('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+app.get('/api/config/paypal', (req, res) =>
+    res.send(process.env.PAYPAL_CLIENT_ID));
+
+const __dirname = path.resolve();
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    })
+}
 
 app.use(notFound);
 
 app.use(errorHandler);
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow.bold));
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+);
